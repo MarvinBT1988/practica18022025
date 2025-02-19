84d5ff660d2a85f5efbf93fd85837360ac4716eb
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-02-19 08:55:20 -0600

    calculadora

---
+++ Practica18022025.AppWebMVC/Controllers/CalculadoraController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica18022025.AppWebMVC.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.num1 = 0;
+            ViewBag.num2 = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calcular(int op, int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            ViewBag.resultado = resultado;
+            ViewBag.num1 = num1;
+            ViewBag.num2 = num2;
+            return View("Index");
+        }
+    }
+}
